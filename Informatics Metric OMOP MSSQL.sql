8cccb3214513af24262df0b4501eb38b78522d84
tjmierzwa <timothy.mierzwa@nih.gov>
2017-08-28 11:18:54 -0400

    Added SQL files

---
+++ Informatics Metric OMOP MSSQL.sql
@@ -0,0 +1,116 @@
+-- Query Informatics Metrics CTSA EDW
+-- Data Model: OMOP 5.X
+-- Database MS SQL
+-- Updated 8/18/2017
+
+-- With Statement used to calculate Unique Patients, used as the denominator for subsequent measures
+with DEN ([Unique Total Patients]) as
+(
+		SELECT CAST(Count(Distinct OP.Person_ID)as Float) as 'Unique Total Patients' 
+		FROM Person OP
+)
+
+--Domain Demographics Unique Patients
+	SELECT 'Demo Unique Patients' AS 'Domain', '' as 'Patients with Standards', 
+	DEN.[Unique Total Patients] as 'Unique Total Patients' ,'' as  '% Standards', 'Not Applicable' as 'Values Present'		
+	FROM DEN	
+
+Union
+-- Domain Gender: % of unique patient with gender populated
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ DEN.[Unique Total Patients])) as '% Standards','Not Applicable' as 'Values Present'	
+	From 
+		(
+		SELECT 'Demo Gender' AS Domain, 
+		CAST(COUNT(DISTINCT D.person_id) as Float) AS 'Patients with Standards'
+		FROM Person D
+		INNER JOIN Concept C ON D.Gender_concept_id = C.concept_id AND C.vocabulary_id = 'Gender' 
+		) Num, DEN
+
+Union
+-- Domain Age/DOBL: % of unique patient with DOB populated
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ Den.[Unique Total Patients])) as '% Standards','Not Applicable' as 'Values Present'	
+	From 
+		(
+		SELECT 'Demo Age/DOB' AS Domain, 
+		CAST(COUNT(DISTINCT D.person_id) as Float) AS 'Patients with Standards'
+		FROM Person D
+		-- We may want to alter this to be only Year of birth present at this time Year, Month and Day are required in order to count
+		Where D.birth_datetime  is NOT NULL 
+			--Where D.Year_of_Birth  is NOT NULL 
+			--and  D.month_of_Birth is NOT NULL 
+			--and  D.Day_of_Birth  is NOT NULL
+		) Num, DEN
+
+Union
+-- Domain Labs: % of unique patient with LOINC as lab valued
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ Den.[Unique Total Patients])) as '% Standards' , 'Not Applicable' as 'Values Present'	
+	From 
+		(
+		SELECT 'Labs as LOINC' AS Domain, 
+		CAST(COUNT(DISTINCT D.person_id) as Float) AS 'Patients with Standards'
+		FROM Measurement D
+		JOIN Concept C ON D.Measurement_concept_id = C.concept_id AND C.vocabulary_id = 'LOINC' 
+		) Num, DEN
+
+Union
+-- Domain Drug: % of unique patient with RxNorm as Medication valued
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ Den.[Unique Total Patients])) as '% Standards','Not Applicable' as 'Values Present'	
+	From 
+		(
+		SELECT 'Drugs as RxNORM' AS Domain, 
+		CAST(COUNT(DISTINCT D.person_id) as Float) AS 'Patients with Standards'
+		FROM DRUG_EXPOSURE D
+		JOIN Concept C ON D.drug_concept_id = C.concept_id AND C.vocabulary_id = 'RxNorm' 
+		) Num, DEN
+Union
+-- Domain Condition: % of unique patient with standard value set for condition
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ Den.[Unique Total Patients])) as '% Standards', 'Not Applicable' as 'Values Present'	
+	From 
+		(
+		SELECT 'Diagnosis as ICD/SNOMED' AS Domain, 
+		CAST(COUNT(DISTINCT P.person_id) as Float) AS 'Patients with Standards' 
+		FROM [Condition_Occurrence] P
+		LEFT JOIN Concept c ON p.condition_source_concept_id = c.concept_id AND c.vocabulary_id IN ('SNOMED','ICD9CM','ICD10CM')
+		LEFT JOIN Concept c2 ON p.condition_concept_id = c2.concept_id AND c2.vocabulary_id IN ('SNOMED','ICD9CM','ICD10CM')
+		WHERE c.concept_id IS NOT NULL OR c2.concept_id IS NOT NULL
+		) Num, DEN
+
+Union
+-- Domain Procedure: % of unique patient with standard value set for procedure
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ Den.[Unique Total Patients])) as '% Standards', 'Not Applicable' as 'Values Present'		
+	From 
+		(
+		SELECT 'Procedures as ICD/SNOMED/CPT4' AS Domain, 
+		CAST(COUNT(DISTINCT P.person_id) as Float) AS 'Patients with Standards'
+		FROM procedure_occurrence P
+		LEFT JOIN Concept c ON p.procedure_source_concept_id= c.concept_id AND c.vocabulary_id IN ('SNOMED','ICD9Proc','ICD10PCS','CPT4')
+		LEFT JOIN Concept c2 ON p.procedure_concept_id = c2.concept_id   AND c2.vocabulary_id IN ('SNOMED','ICD9Proc','ICD10PCS','CPT4')
+		WHERE c.concept_id IS NOT NULL OR c2.concept_id IS NOT NULL
+		) Num, DEN
+
+Union
+-- Domain Observations:  Checks for the presents of recorded observations
+	Select 'Observations Present' AS 'Domain',  '' as 'Patients with Standards', '' as 'Unique Total Patients', '' as  '% Standards', 
+		Case 
+			When Count(*) = 0 then 'No Observation' else 'Observations Present' end as 'Values Present'		
+	from observation
+
+Union
+-- Domain Note Text: % of unique patient with note text populated
+	Select NUM.*, DEN.*, (100.0 * (NUM.[Patients with Standards]/ Den.[Unique Total Patients])) as '% Standards','Not Applicable' as 'Values Present'	
+	From 
+		(
+		SELECT 'Note Text' AS Domain, 
+		CAST(COUNT(DISTINCT D.person_id) as Float) AS 'Patients with Standards'
+		FROM Note D
+		) Num, DEN
+
+----Union
+---- Future Measures 
+-- Domain NLP present does not measure % of unique patients
+--	--Select 'Note NLP Present' AS 'Domain',  '' as 'Patients with Standards', '' as 'Unique Total Patients', '' as  '% Standards', 
+--	--Case 
+--	--		When Count(*) = 0 then 'No Observation' else 'Observations Present' end as 'Values Present'		
+--	--from Note_NLP
+
+Order by Domain
---
+++ Informatics Metric PCORnet Oracle.sql
@@ -0,0 +1,74 @@
+-- Query Informatics Metrics CTSA EDW
+-- Data Model: PCORnet CDM v 3.1
+-- Database Oracle
+-- Updated 8/23/2017
+
+with DEN as 
+  ( select count(*) cnt from demographic )-- CDM standards indicate that demographic should contain one entry per patient. Unique is not required.
+--Domain Demographics Unique Patients
+ select 'Demo Unique Patients' As DOMAIN, null as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, null as Percent_Standards, 'Not Applicable' as Values_Present
+ from den
+union all
+-- Domain Gender: % of unique patient with gender populated
+ select 'Demo Gender' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from (select count(*) cnt from demographic where sex in ('F','M')) num, den -- add 'A' if 'Ambiguous' is acceptable)
+union all
+-- Domain Age/DOBL: % of unique patient with DOB populated
+ select 'Demo Age/DOB' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from (select count(*) cnt from demographic where not birth_date is null) num, den 
+union all
+-- Domain Labs: % of unique patient with LOINC as lab valued
+ select 'Labs as LOINC' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from (select count(distinct patid) cnt from lab_result_cm where not lab_loinc is null) num, den 
+/* union
+-- Domain Drug: % of unique patient with RxNorm as Medication valued
+    -- the CDM Prescribing table for drugs prescribed to the patient has RXNorm CUIs while the Dispensing table has NDC codes. 
+    -- Use this query if NDC is not acceptable
+ select 'Drugs as RxNORM' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from (select count(distinct patid) cnt from prescribing where not rxnorm_cui is null) num, den 
+*/
+union all
+-- Domain Drug: % of unique patient with RxNorm as Medication valued
+    -- the CDM Prescribing table for drugs prescribed to the patient has RXNorm CUIs while the Dispensing table has NDC codes. 
+ select 'Drugs as RxNORM' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from ( select count(distinct patid) cnt
+         from (
+            select distinct patid from prescribing where not rxnorm_cui is null
+            union select distinct patid from dispensing where not ndc is null)
+        ) num, den 
+union all
+-- Domain Condition: % of unique patient with standard value set for condition
+   --CDM separates billing diagnosis (Diagnosis) from problems lists/ patient reported complaints (Condition) 
+select 'Diagnosis as ICD/SNOMED' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from ( select count(distinct patid) cnt 
+         from (select distinct patid from diagnosis where not DX is null and dx_type in ('09', '10', 'SM')
+           union select distinct patid from condition where not condition is null and condition_type in ('09', '10', 'SM'))
+        ) num, den
+/* 
+--Use this query if you only want billing diagnosis.
+select 'Diagnosis as ICD/SNOMED' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from ( select count(distinct patid) cnt 
+         from diagnosis where not DX is null and dx_type in ('09', '10', 'SM')) num, den
+*/
+union all
+-- Domain Procedure: % of unique patient with standard value set for procedure
+      --CDM V3.1 groups CPT and HCPCS
+ select 'Procedures as ICD/SNOMED/CPT4' as domain, num.cnt as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 100.0 * num.cnt/den.cnt as Percent_Standards, 'Not Applicable' as Values_Present
+  from (select count(distinct patid) cnt from procedures where not px is null and px_type in ('09','10','CH')) num, den
+union all
+-- Domain Observations:  Checks for the presents of recorded observations
+ select 'Observations Present' as domain, null as Patients_with_Standards, null as UNIQUE_TOTAL_PATIENTS, null as Percent_Standards, case when num.cnt = 0 then 'No Observation' else 'Observations Present' end as Values_Present
+  from (select count(distinct patid) cnt 
+        from vital 
+        where --Remove observations that are not part of the set (height and weight?)
+           not HT is null or -- height
+           not WT is null or -- weight
+           not diastolic is null or --DBP
+           not systolic is null -- SBP
+        ) num, den
+union all
+-- Domain Note Text: % of unique patient with note text populated
+ -- CDM does not currently have note data.
+ select 'Note Text' as domain, 0 as Patients_with_Standards, den.cnt as UNIQUE_TOTAL_PATIENTS, 0 as Percent_Standards, 'Not Applicable' as Values_Present
+  from den
+order by Domain; 
