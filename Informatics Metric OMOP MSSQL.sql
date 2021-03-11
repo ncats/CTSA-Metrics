672692e3e3ed72a052dbafa7f52e135ba42611d2
Robert Miller <rmiller1@tuftsmedicalcenter.org>
2021-03-11 11:04:39 -0500

    omop mssql version 3 (final?) - pending review
    rework to match specs of 'Final informatics output 2021.xls'

--- Informatics Metric OMOP MSSQL.sql
+++ Informatics Metric OMOP MSSQL.sql
@@ -1,26 +1,107 @@
-
 /*
-CTSA common metrics 2021
-OMOP MSSQL
-Draft 2021/02/22, v2 rewrite for new output structure 
+CTSA common metrics 2021, OMOP MSSQL, version 3
+Draft 2021-03-11, rewrite for third revision of output structure 
 Robert Miller, Tufts CTSI 
-
 Based on criteria from: 
-https://github.com/ncats/CTSA-Metrics/blob/master/ProposedClicMetricTable.xlsx
-and "Informatics 2.4.21.docx" (email distributed) 
+	https://github.com/ncats/CTSA-Metrics/blob/master/ProposedClicMetricTable.xlsx
+	and "Final Informatics Output 2021.xls" (email distributed) 
 
 Known issues:
--missing smoking concept list
--missing vital sign concept list (OMOP doesn't have this table)
--unclear approach to consider insurance dates overlapping (within 1yr, 5yr)
--event name ('2020_arm_1') now missing from criteria (removed?)
--nlp solutions now missing from criteria (removed?)
+- hardwires uniq_pt_insurance_value_set to NULL as it is unclear how to best qualify insurance providers as 'harmonized' in OMOP 
+
+Structure:
+1-user input
+	nlp solutions **Needs to be edited by user (0=no, 1=yes)**
+2-create concept sets
+3-generate metrics 
 
 */
 
 
+
+
+-- Are you using an NLP solution at your site? 
+with nlp_usage as(
+	SELECT -1 as edit_me --[ 0 = No, 1 = Yes ]
+)
+
+,vital_concepts as (
+	/*
+	Spec: At least one vital: height, weight, blood pressure, BMI, or temperature
+
+	 height
+		3036277
+	 , weight
+		3025315
+	 , blood pressure
+		45876174
+	 , BMI
+		1002813
+		4245997
+	 , temperature
+		1004059
+		4178505
+
+	# concepts exist in both meas and obs
+
+*/
+
+	SELECT conc.*
+	FROM CONCEPT_ANCESTOR ca
+	INNER JOIN CONCEPT conc
+	ON ancestor_concept_id IN
+	(
+		3036277
+		,3025315
+		,45876174
+		,1002813
+		,4245997
+		,1004059
+		,4178505
+	)
+	AND ca.descendant_concept_id = conc.concept_id
+	AND conc.standard_concept = 'S'
+)
+
+,smoking_concepts as (
+
+	SELECT conc.*
+	FROM CONCEPT_RELATIONSHIP cr 
+	INNER JOIN CONCEPT conc
+	ON cr.concept_id_1 IN
+	(
+		SELECT concept_id
+		FROM CONCEPT
+		WHERE concept_code in 
+		(
+			'Z87.891'
+			,'F17.20'
+			,'F17.200'
+			, 'F17.201'
+			, 'F17.203'
+			, 'F17.208'
+			, 'F17.209'
+			, 'F17.21'
+			, 'F17.210'
+			, 'F17.211'
+			, 'F17.213'
+			, 'F17.218'
+			, 'F17.219'
+		)
+	)
+	AND cr.concept_id_2 = conc.concept_id
+	AND conc.standard_concept = 'S'
+
+)
+
+SELECT
+	'nlp_any' as variable_name
+	,(SELECT edit_me FROM nlp_usage) as one_year
+	,(SELECT edit_me FROM nlp_usage) as five_year
+
+UNION
 SELECT 
-	'total_encounters' as x
+	'total_encounters' as variable_name
 	,(
 		SELECT COUNT(*) 
 		FROM visit_occurrence
@@ -34,7 +115,7 @@ SELECT
 
 UNION 
 SELECT 
-	'total_patients' as x
+	'total_patients' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id) 
 		FROM visit_occurrence
@@ -48,7 +129,7 @@ SELECT
 
 UNION 
 SELECT 
-	'unique_pt_with_age' as x
+	'unique_pt_with_age' as variable_name
 	,(
 		SELECT COUNT(DISTINCT per.person_id) 
 		FROM person per
@@ -68,7 +149,7 @@ SELECT
 
 UNION 
 SELECT 
-	'unique_pt_birthdate_in_future' as x
+	'unique_pt_birthdate_in_future' as variable_name
 	,(
 		SELECT COUNT(DISTINCT per.person_id) 
 		FROM person per
@@ -88,7 +169,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_age_over_120' as x
+	'uniq_pt_age_over_120' as variable_name
 	,(
 		SELECT COUNT(DISTINCT per.person_id) 
 		FROM 
@@ -134,7 +215,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_with_gender' as x
+	'uniq_pt_with_gender' as variable_name
 	,(
 		SELECT COUNT(DISTINCT per.person_id) 
 		FROM person per
@@ -164,7 +245,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_loinc' as x
+	'uniq_pt_loinc' as variable_name
 	,(
 		SELECT COUNT(DISTINCT per.person_id) 
 		FROM (
@@ -207,7 +288,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_med_rxnorm' as x
+	'uniq_enc_med_rxnorm' as variable_name
 	,(
 		SELECT COUNT(DISTINCT drug.visit_occurrence_id) 
 		FROM drug_exposure drug
@@ -227,7 +308,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_med_rxnorm' as x
+	'uniq_pt_med_rxnorm' as variable_name
 	,(
 		SELECT COUNT(DISTINCT drug.person_id) 
 		FROM drug_exposure drug
@@ -247,7 +328,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_icd_dx' as x
+	'uniq_pt_icd_dx' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM condition_occurrence
@@ -279,7 +360,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_icd_dx' as x
+	'uniq_enc_icd_dx' as variable_name
 	,(
 		SELECT COUNT(DISTINCT visit_occurrence_id)
 		FROM condition_occurrence
@@ -311,7 +392,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_snomed_dx' as x
+	'uniq_pt_snomed_dx' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM condition_occurrence
@@ -343,7 +424,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_snomed_dx' as x
+	'uniq_enc_snomed_dx' as variable_name
 	,(
 		SELECT COUNT(DISTINCT visit_occurrence_id)
 		FROM condition_occurrence
@@ -376,7 +457,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_icd_proc' as x
+	'uniq_pt_icd_proc' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM procedure_occurrence
@@ -408,7 +489,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_icd_proc' as x
+	'uniq_enc_icd_proc' as variable_name
 	,(
 		SELECT COUNT(DISTINCT visit_occurrence_id)
 		FROM procedure_occurrence
@@ -442,7 +523,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_cpt' as x
+	'uniq_pt_cpt' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM procedure_occurrence
@@ -474,7 +555,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_cpt' as x
+	'uniq_enc_cpt' as variable_name
 	,(
 		SELECT COUNT(DISTINCT visit_occurrence_id)
 		FROM procedure_occurrence
@@ -507,7 +588,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_snomed_proc' as x
+	'uniq_pt_snomed_proc' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM procedure_occurrence
@@ -539,7 +620,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_snomed_proc' as x
+	'uniq_enc_snomed_proc' as variable_name
 	,(
 		SELECT COUNT(DISTINCT visit_occurrence_id)
 		FROM procedure_occurrence
@@ -572,7 +653,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_note' as x
+	'uniq_pt_note' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM note
@@ -586,7 +667,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_enc_note' as x
+	'uniq_enc_note' as variable_name
 	,(
 		SELECT COUNT(DISTINCT visit_occurrence_id)
 		FROM note
@@ -598,29 +679,141 @@ SELECT
 		WHERE note_date BETWEEN '01-01-2016' AND '12-31-2020'
 	) as five_year
 
--- There isn't a vital sign table in OMOP 
--- Either need list of concepts to search for or leave null as below 
+
+
+/*
+Spec: At least one vital: height, weight, blood pressure, BMI, or temperature
+
+ height
+	3036277
+ , weight
+	3025315
+ , blood pressure
+	45876174
+ , BMI
+	1002813
+	4245997
+ , temperature
+	1004059
+	4178505
+*/
 UNION 
 SELECT 
-	'uniq_pt_vital_sign' as x
-	,-1 as one_year
-	,-1 as five_year
+	'uniq_pt_vital_sign' as variable_name
+	,(
+		SELECT COUNT(distinct person_id) 
+		FROM
+		(
+			SELECT person_id 
+			FROM MEASUREMENT
+			WHERE measurement_concept_id IN 
+			(
+				SELECT concept_id
+				FROM vital_concepts
+				WHERE domain_id = 'Measurement'
+			)
+			AND measurement_date BETWEEN '01-01-2020' AND '12-31-2020'
+			UNION 
+			SELECT person_id 
+			FROM OBSERVATION
+			WHERE observation_concept_id IN 
+			(
+				SELECT concept_id
+				FROM vital_concepts
+				WHERE domain_id = 'Observation'
+			)
+			AND observation_date BETWEEN '01-01-2020' AND '12-31-2020'
+		) x
+	) as one_year
+	,(
+		SELECT COUNT(distinct person_id) 
+		FROM
+		(
+			SELECT person_id 
+			FROM MEASUREMENT
+			WHERE measurement_concept_id IN 
+			(
+				SELECT concept_id
+				FROM vital_concepts
+				WHERE domain_id = 'Measurement'
+			)
+			AND measurement_date BETWEEN '01-01-2016' AND '12-31-2020'
+			UNION 
+			SELECT person_id 
+			FROM OBSERVATION
+			WHERE observation_concept_id IN 
+			(
+				SELECT concept_id
+				FROM vital_concepts
+				WHERE domain_id = 'Observation'
+			)
+			AND observation_date BETWEEN '01-01-2016' AND '12-31-2020'
+		) x
+	) as five_year
 
 
--- Either need list of concepts to search for or leave null as below 
+/*
+Smoking Status Codes: Z87.891, F17.20, F17.200, F17.201, F17.203, F17.208, F17.209, F17.21, F17.210, F17.211, F17.213, F17.218, F17.219
+*/
 UNION 
 SELECT 
-	'uniq_pt_smoking' as x
-	,-1 as one_year
-	,-1 as five_year
-
-
+	'uniq_pt_smoking' as variable_name
+	,(
+		SELECT COUNT(distinct person_id) 
+		FROM
+		(
+			SELECT person_id 
+			FROM CONDITION_OCCURRENCE
+			WHERE condition_concept_id IN 
+			(
+				SELECT concept_id
+				FROM smoking_concepts
+				WHERE domain_id = 'Condition'
+			)
+			AND condition_start_date BETWEEN '01-01-2020' AND '12-31-2020'
+			UNION 
+			SELECT person_id 
+			FROM OBSERVATION
+			WHERE observation_concept_id IN 
+			(
+				SELECT concept_id
+				FROM smoking_concepts
+				WHERE domain_id = 'Observation'
+			)
+			AND observation_date BETWEEN '01-01-2020' AND '12-31-2020'
+		) x
+	) as one_year
+	,(
+		SELECT COUNT(distinct person_id) 
+		FROM
+		(
+			SELECT person_id 
+			FROM CONDITION_OCCURRENCE
+			WHERE condition_concept_id IN 
+			(
+				SELECT concept_id
+				FROM smoking_concepts
+				WHERE domain_id = 'Condition'
+			)
+			AND condition_start_date BETWEEN '01-01-2016' AND '12-31-2020'
+			UNION 
+			SELECT person_id 
+			FROM OBSERVATION
+			WHERE observation_concept_id IN 
+			(
+				SELECT concept_id
+				FROM smoking_concepts
+				WHERE domain_id = 'Observation'
+			)
+			AND observation_date BETWEEN '01-01-2016' AND '12-31-2020'
+		) x
+	) as five_year
 
 
 
 UNION 
 SELECT 
-	'uniq_pt_opioid' as x
+	'uniq_pt_opioid' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM
@@ -686,7 +879,7 @@ SELECT
 
 UNION 
 SELECT 
-	'uniq_pt_insurance_value_set' as x
+	'uniq_pt_any_insurance_value' as variable_name
 	,(
 		SELECT COUNT(DISTINCT person_id)
 		FROM payer_plan_period
@@ -700,15 +893,11 @@ SELECT
 		OR payer_plan_period_end_date BETWEEN '01-01-2016' AND '12-31-2020'
 	) as five_year
 
+-- NULL as NA. Best approach TBD for OMOP 
+UNION 
+SELECT 
+	'uniq_pt_insurance_value_set' as variable_name
+	,NULL as one_year
+	,NULL as five_year
 
-/*
- Excluded as it sounded like we are getting rid of the following
-
-	uniq_pt_clin_vis_anytime: 
-		unique patients with at least one clinical visit cpt code from list/encounter type	
-	uniq_pt_clin_vis_timeframe:
-		unique patients with at least one clinical visit cpt code from list/encounter type in time frame	
-
-*/
-
-ORDER BY x
+;
